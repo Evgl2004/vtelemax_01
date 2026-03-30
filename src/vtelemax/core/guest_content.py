7fae9acd4afce2486453847f151ed61d89f00c21
admin_eas <a.evgl@yandex.ru>
2026-03-30 14:54:22 +0500

    Исправлено отображение ссылки отзыва для VK и унифицирован текст

--- src/vtelemax/core/guest_content.py
+++ src/vtelemax/core/guest_content.py
@@ -348,12 +348,12 @@ def build_support_feedback_screen() -> MenuScreenContract:
     return MenuScreenContract(
         screen_id="support_feedback",
         text=(
-            "✍️ *Оставить отзыв*\n\n"
+            "✍️ Оставить отзыв\n\n"
             "Мы будем рады узнать ваше мнение.\n"
-            "👉 [Оставить отзыв!](https://rdata.one/Nyyl)"
+            "👉 Оставить отзыв: https://rdata.one/Nyyl"
         ),
         buttons=(MenuButtonContract(action=GuestMenuAction.BACK_TO_SUPPORT, label=BUTTON_BACK_TO_SUPPORT),),
-        parse_mode="markdown",
+        parse_mode="plain",
     )
 
 
